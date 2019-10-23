80076cebac92aada96ba64e9a938e09dfa4ee688
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-10-23 18:15:51 -0400

    Update bfa.sh

--- bfa.sh
+++ bfa.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-chmod +x bFA.sh
 
-echo "BFA.SH --- YOUR BUDGET FILING ASSISTANT"
+echo "BFA.SH - YOUR BUDGET FILING ASSISTANT"
 echo "https://github.com/ForgivenNin/BudgetLogTemplate/"
+echo "-----"
+echo "This program helps you manage all of your receipts on a timely basis."
 
 # User inputs file
-echo "What file would you like to use?
+echo "Is there a file you would like to register?"
 read FILENAME
 
 # Logs file modification/creation time
 stat -c %y "~$FILENAME"
 
 echo "STORED $FILENAME"
+
+
+# Run in background to manage files
+bkr() {(nohup "$@" &>/dev/null &)}
+bkr ./bfa.sh
